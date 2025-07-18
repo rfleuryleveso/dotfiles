0a9bd1ea8adb95080b068bb3ab63019e8a098cd6
Raphaël FLEURY-LE VESO <raphael.fleury-le-veso@stormshield.eu>
2025-07-18 16:37:20 +0200

    Updates

--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -6,14 +6,6 @@ return {
 			globalstatus = vim.o.laststatus == 3,
 			disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
 		},
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { "branch" },
-
-			lualine_c = {
-				{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-			},
-		},
 	},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 }
